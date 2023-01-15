1975831d9f7f270ceef3424b1341d3ece889bf02
sivaaravindh <sivaaravind262003@gmail.com>
2023-01-15 12:06:20 +0530

    index

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let a=parseInt(prompt("enter your age"));
+let b=parseInt(prompt("enter your age"));
+let c=parseInt(prompt("enter your age"));
+
+if(a>b){
+	if(a>c){
+		console.log ("a is bigger")
+		}
+	else{
+		console.log ("c is bigger")
+	}
+}
+else if(b>c){
+	console.log ("b is bigger")
+	}
+else{
+	console.log ("c is bigger")
+}
+
+
+
+
